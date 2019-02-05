d29b8b24d92c182fc6085ff16b68fcda3dfdc86c
Mike Duin <michael.s.duin@gmail.com>
2019-02-04 22:05:45 -0800

    wire up create stream action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,3 +1,4 @@
+import streams from '../apis/streams';
 import { SIGN_IN, SIGN_OUT } from './types';
 
 export const signIn = (userId) => {
@@ -11,4 +12,9 @@ export const signOut = () => {
   return {
     type: SIGN_OUT
   }
+}
+
+// This action creator will be called with a list of values that were entered into our form as an argument
+export const createStream = formValues => async dispatch => {
+  streams.post('/streams', formValues);
 }
